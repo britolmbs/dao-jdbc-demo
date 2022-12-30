8ca7accf613432136d7f3ed3a67c6c687981c053
britolmbs <britolmbs@hotmail.com>
2022-12-30 14:48:37 -0300

    Projeto Dao

---
+++ src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.enteties.Department;
+import model.enteties.Seller;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
+
+        System.out.println("=======TEST FINDBYDEPARTMENT==========");
+
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+        System.out.println("========TEST FINDALL=======");
+        list = sellerDao.findAll();
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
+
+        System.out.println("=======TEST INSERT==========");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+         SellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("=======TEST UPDATE=========");
+
+        seller = sellerDao.findById(1);
+        seller.setName("Martha waine");
+        sellerDao.update(seller);
+        System.out.println("Update.");
+
+
+
+        System.out.println("========TEST DELETE=====");
+        System.out.println("enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete!");
+        sc.close();
+    }
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import model.enteties.Department;
+import model.enteties.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+    List<Seller> findByDepartment(Department department);
+}
